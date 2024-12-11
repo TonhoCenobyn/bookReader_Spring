8a7693767e89a415c7a606bfe087bdf9ef5a8993
Marco Antonio <127147646+TonhoCenobyn@users.noreply.github.com>
2024-12-10 21:13:42 -0300

    Delete bookReader directory

--- bookReader/src/main/resources/db/migration/V1__create-table_cliente.sql
+++
@@ -1,7 +0,0 @@
-create table clientes(
-                         id BIGSERIAL not null primary key,
-                         uuid UUID DEFAULT gen_random_uuid(),
-                         nome varchar(50) not null,
-                         email varchar(100) not null unique,
-                         senha varchar(50) not null unique
-)
--- bookReader/src/main/resources/db/migration/V2__create-table-livro.sql
+++
@@ -1,7 +0,0 @@
-create table livros(
-                         id BIGSERIAL not null primary key,
-                         uuid UUID DEFAULT gen_random_uuid(),
-                         titulo varchar(50) not null,
-                         autor varchar(100) not null unique
-);
-
